cf17979aab709a707e6c3b40460cebb3f9ae6046
Karen <karen.trainingaitc@gmail.com>
2018-11-12 17:54:53 +0400

    test 1

--- js/script.js
+++ js/script.js
@@ -1,48 +1,43 @@
 $( document ).ready(function() {
-	function random_count(){
-		var star_count = Math.floor((Math.random() * 20) + 10);
-		for (var i = 0; i <= star_count; i++) {
-			coords();
-		}
-	}
-	function coords(){
-		var window_width = $(window).width()-40;
-		var window_height = $(window).height()-40;
-		var random_x = Math.floor((Math.random() * window_width) + 1);
-		var random_y = Math.floor((Math.random() * window_height) + 1);
+	
+	setInterval(function(){
+		make_stars();
+		remove_stars();
+	}, 1000);
 
-		var star_size_random = Math.floor((Math.random() * 5) + 1);
-
-		$('body').prepend('<p class="stexcvac">&#9733;</p>');
-		$(".stexcvac").css({
-			"top" : random_y,
-			"left" : random_x,
-			"opacity" : "1",
-		})
-		$(".stexcvac").fadeTo(1500, 0);
-		$(".stexcvac").attr("class","est");
-
-	}
-	var myVar = setInterval(function(){ create_stars() }, 500);
-	function create_stars() {
-		random_count();
-	}
-
-	var myVar1 = setInterval(function(){ opacity_0() }, 500);
-	var g = "";
-	function opacity_0() {
-		var est = $(".est");
-		var length = est.length;
-		if (est.length != 0) {
-			for (var i = 0; i < length; i++) {
-				g = document.getElementsByClassName('est')[i].style.opacity;
-		    	if (g == 0) {
-		    		est[i].remove();
-		    		est = $(".est");
-		    		length = est.length;
-		    	}
-			}
-		}
-	    
-	}
 });
+
+
+function make_stars(){
+	var star_count = Math.floor((Math.random() * 40) + 10);
+	for (var i = 0; i <= star_count; i++) {
+		coords();
+	}
+}
+
+function coords(){
+	var window_width = $(window).width()-40;
+	var window_height = $(window).height()-40;
+	var x = Math.floor((Math.random() * window_width) + 1);
+	var y = Math.floor((Math.random() * window_height) + 1);
+
+	$('body').prepend('<p class="star-fade">&#9733;</p>');
+	$(".star-fade").css({
+		"top" : y,
+		"left" : x
+	}),
+	$(".star-fade").fadeTo(3000, 0);
+	$(".star-fade").attr("class","star");
+}
+
+function remove_stars() {
+	var star_count = $('.star').length;
+	if (star_count != 0) {
+		for (var i = 0; i < star_count; i++) {
+			var opacity = $('.star:eq('+i+')').css("opacity");
+	    	if (opacity == 0) {
+	    		$('.star:eq('+i+')').remove();
+	    	}
+		}
+	}
+}
