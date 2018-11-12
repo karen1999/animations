6346dc6d476a8ed88b6c6f9c660892cb4b15e516
Karen <karen.trainingaitc@gmail.com>
2018-11-12 22:12:57 +0400

    Update follow-the-mouse/index.php

---
+++ follow-the-mouse/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ru" >
+    <head>
+        <title>Գլխավոր</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    </head>
+    <body>
+    <div class="test">
+      <div id="mouse"></div>
+    </div>  
+    <script src="script.js"></script>
+    </body>
+</html>
